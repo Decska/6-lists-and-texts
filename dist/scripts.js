49fa0ff141e1a4c76f1fbe63c3d489f64f093362
Decska <66358871+Decska@users.noreply.github.com>
2020-06-17 20:05:56 +0200

    first commit

---
+++ dist/scripts.js
@@ -0,0 +1,12 @@
+/*let myArray = ["Payrits", "Pénztáros", "Kertész", "Járfás", "Halász", "Decsényi"];
+
+myArray.forEach.(('ul') => {.append(myArray)})*/
+
+
+  $('body').append(
+    '<h1>Added with Javascript</h1>'  );
+  $('body').append("This block was added using JavaScript's jQuery library. How awesome!");
+
+
+ 
+  
